9b20918e82a2b06c9c182a3874881c0f0d93de09
Taine Reader <taine.reader@gmail.com>
2024-12-04 23:42:42 +1300

    missed express lol

---
+++ test_client.py
@@ -0,0 +1,24 @@
+import websockets
+import uuid
+import asyncio
+
+print("Testing Websockets")
+
+async def ws_loop():
+    uri = "ws://invalidse-wifi-lights.host.qrl.nz/"
+    async with websockets.connect(uri) as websocket:
+        print("Connected to server")
+        await websocket.send(uuid.uuid4().hex)
+        while True:
+            message = await websocket.recv()
+            print(f"Received message: {message}")
+
+
+if __name__ == "__main__":
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    loop.run_until_complete(ws_loop())
+    loop.close()
+
+
+    
